bc44a5069e6086ccac99f5bd59b2628b1756a74c
Vinicius Colasanto <vscolasanto@gmail.com>
2024-08-02 21:59:29 -0300

    feat: add create-author.interface and author.repository interface

---
+++ src/authors/interfaces/author.repository.ts
@@ -0,0 +1,31 @@
+import { Author } from '../graphql/models/author'
+import { ICreateAuthor } from './create-author'
+
+export type SearchParams = {
+  page?: number
+  perPage?: number
+  filer?: string
+  sort?: string
+  sortDir?: 'asc' | 'desc'
+}
+
+export type SearchResult = {
+  items: Author[]
+  currentPage: number
+  perPage: number
+  lastPage: number
+  total: number
+}
+
+export interface IAuthorsRepository {
+  sortableFields: string[]
+  create(data: ICreateAuthor): Promise<Author>
+  update(author: Author): Promise<Author>
+  delete(id: string): Promise<Author>
+  findById(id: string): Promise<Author>
+  findAll(): Promise<Author[]>
+  findByName(name: string): Promise<Author[]>
+  findByEmail(email: string): Promise<Author[]>
+  search(params: SearchParams): Promise<SearchResult>
+  get(id: string): Promise<Author>
+}

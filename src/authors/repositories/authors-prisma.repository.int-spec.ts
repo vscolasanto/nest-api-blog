44f31368821100696ecdc2c27c5330642f7f90fa
Vinicius Colasanto <vscolasanto@gmail.com>
2024-08-03 21:50:09 -0300

    feat: implement search method tests

--- src/authors/repositories/authors-prisma.repository.int-spec.ts
+++ src/authors/repositories/authors-prisma.repository.int-spec.ts
@@ -25,7 +25,7 @@ describe('AuthorsPrismaRepository Integration Tests', () => {
     await module.close()
   })
 
-  describe('should test findById method', () => {
+  describe('findById method', () => {
     test('should throws not found error when id is not found', async () => {
       const uuid = '76d94a09-46a7-4cf5-be68-e8528e510f37'
       expect(repository.findById(uuid)).rejects.toThrow(
@@ -41,11 +41,123 @@ describe('AuthorsPrismaRepository Integration Tests', () => {
     })
   })
 
-  describe('should test create method', () => {
+  describe('create method', () => {
     test('should create an author', async () => {
       const data = AuthorDataBuilder({})
       const author = await repository.create(data)
       expect(author).toMatchObject(data)
+    })
+  })
+
+  describe('search method', () => {
+    test('should apply default params when params are not provided', async () => {
+      const createAt = new Date()
+      const data = []
+      const arrange = Array(16).fill(AuthorDataBuilder({}))
+      arrange.forEach((element, index) => {
+        const timestamp = createAt.getTime() + index
+        data.push({
+          ...element,
+          email: `author${index}@a.com`,
+          createdAt: new Date(timestamp),
+        })
+      })
+
+      await prisma.author.createMany({ data })
+      const result = await repository.search({})
+      expect(result.total).toBe(16)
+      expect(result.items.length).toBe(15)
+      result.items.forEach(item => {
+        expect(item.id).toBeDefined()
+      })
+      result.items.reverse().forEach((item, index) => {
+        expect(`${item.email}${index + 1}@a.com`)
+      })
+    })
+
+    test('should apply pagination and ordering', async () => {
+      const createAt = new Date()
+      const data = []
+      const arrange = 'gbafecd'
+      arrange.split('').forEach((element, index) => {
+        const timestamp = createAt.getTime() + index
+        data.push({
+          ...AuthorDataBuilder({ name: element }),
+          email: `author${index}@a.com`,
+          createdAt: new Date(timestamp),
+        })
+      })
+
+      await prisma.author.createMany({ data })
+      const page1 = await repository.search({
+        page: 1,
+        perPage: 2,
+        sort: 'name',
+        sortDir: 'asc',
+      })
+      expect(page1.items[0]).toMatchObject(data[2])
+      expect(page1.items[1]).toMatchObject(data[1])
+
+      const page2 = await repository.search({
+        page: 2,
+        perPage: 2,
+        sort: 'name',
+        sortDir: 'asc',
+      })
+      expect(page2.items[0]).toMatchObject(data[5])
+      expect(page2.items[1]).toMatchObject(data[6])
+
+      const page3 = await repository.search({
+        page: 3,
+        perPage: 2,
+        sort: 'name',
+        sortDir: 'asc',
+      })
+      expect(page3.items[0]).toMatchObject(data[4])
+      expect(page3.items[1]).toMatchObject(data[3])
+
+      const page4 = await repository.search({
+        page: 4,
+        perPage: 2,
+        sort: 'name',
+        sortDir: 'asc',
+      })
+      expect(page4.items[0]).toMatchObject(data[0])
+    })
+
+    test('should apply pagination, filtering and ordering', async () => {
+      const createAt = new Date()
+      const data = []
+      const arrange = ['test', 'a', 'TEST', 'b', 'Test']
+      arrange.forEach((element, index) => {
+        const timestamp = createAt.getTime() + index
+        data.push({
+          ...AuthorDataBuilder({ name: element }),
+          email: `author${index}@a.com`,
+          createdAt: new Date(timestamp),
+        })
+      })
+
+      await prisma.author.createMany({ data })
+      const page1 = await repository.search({
+        page: 1,
+        perPage: 2,
+        sort: 'name',
+        sortDir: 'asc',
+        filter: 'TEST',
+      })
+      expect(page1.items[0]).toMatchObject(data[0])
+      expect(page1.items[1]).toMatchObject(data[4])
+
+      const page2 = await repository.search({
+        page: 2,
+        perPage: 2,
+        sort: 'name',
+        sortDir: 'asc',
+        filter: 'TEST',
+      })
+      expect(page2.items[0]).toMatchObject(data[2])
+      expect(page2.items.length).toBe(1)
     })
   })
 })

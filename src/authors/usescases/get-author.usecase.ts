4bfd67329f775b119ab3a27ef2adb9976f0f4990
Vinicius Colasanto <vscolasanto@gmail.com>
2024-08-05 14:06:43 -0300

    feat: implement GetAuthorUsecase

---
+++ src/authors/usescases/get-author.usecase.ts
@@ -0,0 +1,28 @@
+import { BadRequestError } from '@/shared/errors/bad-request-error'
+import { AuthorsPrismaRepository } from '../repositories/authors-prisma.repository'
+import { IUsecase } from '@/shared/interfaces/usecase.interface'
+
+export namespace GetAuthorUsecase {
+  type Base = {
+    id: string
+  }
+
+  export type Input = Base
+
+  export type Output = Base & {
+    name: string
+    email: string
+    createdAt: Date
+  }
+
+  export class Usecase implements IUsecase<Input, Output> {
+    constructor(private authorsRepository: AuthorsPrismaRepository) {}
+
+    async execute(input: Input): Promise<Output> {
+      const { id } = input
+      if (!id) throw new BadRequestError('Id should be provided')
+      const author = await this.authorsRepository.findById(id)
+      return author
+    }
+  }
+}

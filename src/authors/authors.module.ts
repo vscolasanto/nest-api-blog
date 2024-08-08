2fd488518458e42fdad80fbb29e3395d944ceb66
Vinicius Colasanto <vscolasanto@gmail.com>
2024-08-08 19:54:46 -0300

    feat: add all authors usecases in authors module

--- src/authors/authors.module.ts
+++ src/authors/authors.module.ts
@@ -4,6 +4,10 @@ import { DatabaseModule } from '@/database/database.module'
 import { PrismaService } from '@/database/prisma/prisma.service'
 import { AuthorsPrismaRepository } from './repositories/authors-prisma.repository'
 import { ListAuthorsUsecase } from './usescases/list-authors.usecase'
+import { GetAuthorUsecase } from './usescases/get-author.usecase'
+import { CreateAuthorUsecase } from './usescases/create-author.usecase'
+import { UpdateAuthorUsecase } from './usescases/update-author.usecase'
+import { DeleteAuthorUsecase } from './usescases/delete-author.usecase'
 
 @Module({
   imports: [DatabaseModule],
@@ -24,6 +28,34 @@ import { ListAuthorsUsecase } from './usescases/list-authors.usecase'
       provide: ListAuthorsUsecase.Usecase,
       useFactory: (authorsRepository: AuthorsPrismaRepository) => {
         return new ListAuthorsUsecase.Usecase(authorsRepository)
+      },
+      inject: ['AuthorsRepository'],
+    },
+    {
+      provide: GetAuthorUsecase.Usecase,
+      useFactory: (authorsRepository: AuthorsPrismaRepository) => {
+        return new GetAuthorUsecase.Usecase(authorsRepository)
+      },
+      inject: ['AuthorsRepository'],
+    },
+    {
+      provide: CreateAuthorUsecase.Usecase,
+      useFactory: (authorsRepository: AuthorsPrismaRepository) => {
+        return new CreateAuthorUsecase.Usecase(authorsRepository)
+      },
+      inject: ['AuthorsRepository'],
+    },
+    {
+      provide: UpdateAuthorUsecase.Usecase,
+      useFactory: (authorsRepository: AuthorsPrismaRepository) => {
+        return new UpdateAuthorUsecase.Usecase(authorsRepository)
+      },
+      inject: ['AuthorsRepository'],
+    },
+    {
+      provide: DeleteAuthorUsecase.Usecase,
+      useFactory: (authorsRepository: AuthorsPrismaRepository) => {
+        return new DeleteAuthorUsecase.Usecase(authorsRepository)
       },
       inject: ['AuthorsRepository'],
     },

ec6803a23bd9b4fc87fd5f212789e7b8dc3429e4
Vinicius Colasanto <vscolasanto@gmail.com>
2024-08-07 22:06:26 -0300

    feat: implement DeleteAuthorUsecase

---
+++ src/authors/usescases/delete-author.usecase.ts
@@ -0,0 +1,23 @@
+import { BadRequestError } from '@/shared/errors/bad-request-error'
+import { AuthorsPrismaRepository } from '../repositories/authors-prisma.repository'
+import { IUsecase } from '@/shared/interfaces/usecase.interface'
+import { AuthorOutput } from '../dt/author-output.dto'
+
+export namespace DeleteAuthorUsecase {
+  type Input = {
+    id: string
+  }
+
+  export type Output = AuthorOutput
+
+  export class Usecase implements IUsecase<Input, Output> {
+    constructor(private authorsRepository: AuthorsPrismaRepository) {}
+
+    async execute(input: Input): Promise<Output> {
+      const { id } = input
+      if (!id) throw new BadRequestError('Id should be provided')
+      const author = await this.authorsRepository.delete(id)
+      return author
+    }
+  }
+}

d6bb81be739a90fd6c6b44b790724f6554850667
Vinicius Colasanto <vscolasanto@gmail.com>
2024-08-09 19:33:13 -0300

    feat: implement GetAuthorUsecase, UpdateAuthorUsecase and DeleteAuthorUsecase in AuthorsResolve

--- src/authors/graphql/resolvers/authors.resolver.ts
+++ src/authors/graphql/resolvers/authors.resolver.ts
@@ -6,14 +6,28 @@ import { SearchParamsArgs } from '../args/search-params.args'
 import { SearchAuthorResult } from '../models/search-author-result'
 import { CreateAuthorUsecase } from '@/authors/usescases/create-author.usecase'
 import { CreateAuthorInput } from '../inputs/create-author.input'
+import { GetAuthorUsecase } from '@/authors/usescases/get-author.usecase'
+import { AuthorIdArgs } from '../args/author-id.args'
+import { UpdateAuthorUsecase } from '@/authors/usescases/update-author.usecase'
+import { UpdateAuthorInput } from '../inputs/update-author.input'
+import { DeleteAuthorUsecase } from '@/authors/usescases/delete-author.usecase'
 
 @Resolver(() => Author)
 export class AuthorsResolver {
   @Inject(ListAuthorsUsecase.Usecase)
   private listAuthorsUsecase: ListAuthorsUsecase.Usecase
 
+  @Inject(GetAuthorUsecase.Usecase)
+  private getAuthorUsecase: GetAuthorUsecase.Usecase
+
   @Inject(CreateAuthorUsecase.Usecase)
   private createAuthorUsecase: CreateAuthorUsecase.Usecase
+
+  @Inject(UpdateAuthorUsecase.Usecase)
+  private updateAuthorUsecase: UpdateAuthorUsecase.Usecase
+
+  @Inject(DeleteAuthorUsecase.Usecase)
+  private deleteAuthorUsecase: DeleteAuthorUsecase.Usecase
 
   @Query(() => SearchAuthorResult)
   async authors(
@@ -28,8 +42,26 @@ export class AuthorsResolver {
     })
   }
 
+  @Query(() => Author)
+  async getAuthorById(@Args() { id }: AuthorIdArgs) {
+    return this.getAuthorUsecase.execute({ id })
+  }
+
   @Mutation(() => Author)
-  createAuthor(@Args('data') data: CreateAuthorInput) {
+  async createAuthor(@Args('data') data: CreateAuthorInput) {
     return this.createAuthorUsecase.execute(data)
+  }
+
+  @Mutation(() => Author)
+  async UpdateAuthor(
+    @Args() { id }: AuthorIdArgs,
+    @Args('data') data: UpdateAuthorInput,
+  ) {
+    return this.updateAuthorUsecase.execute({ id, ...data })
+  }
+
+  @Mutation(() => Author)
+  async deleteAuthor(@Args() { id }: AuthorIdArgs) {
+    return this.deleteAuthorUsecase.execute({ id })
   }
 }

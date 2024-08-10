d779a4e5208b15479ee282341a38ae38f2da9efc
Vinicius Colasanto <vscolasanto@gmail.com>
2024-08-10 15:37:07 -0300

    feat: add type validation in input and args files

--- src/authors/graphql/inputs/create-author.input.ts
+++ src/authors/graphql/inputs/create-author.input.ts
@@ -1,10 +1,16 @@
 import { Field, InputType } from '@nestjs/graphql'
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator'
 
 @InputType()
 export class CreateAuthorInput {
+  @IsString()
+  @IsNotEmpty()
   @Field()
   name: string
 
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
   @Field()
   email: string
 }
--- src/authors/graphql/inputs/update-author.input.ts
+++ src/authors/graphql/inputs/update-author.input.ts
@@ -1,10 +1,16 @@
 import { Field, InputType } from '@nestjs/graphql'
+import { IsEmail, IsOptional, IsString } from 'class-validator'
 
 @InputType()
 export class UpdateAuthorInput {
+  @IsString()
+  @IsOptional()
   @Field({ nullable: true })
   name: string
 
+  @IsString()
+  @IsOptional()
+  @IsEmail()
   @Field({ nullable: true })
   email: string
 }

e4bf56e2bb44717cb7f1fc4f517f72026544153c
Vinicius Colasanto <vscolasanto@gmail.com>
2024-08-13 20:00:19 -0300

    feat: create PostsPrismaRepository

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,4 +1,22 @@
-import { Module } from '@nestjs/common';
+import { DatabaseModule } from '@/database/database.module'
+import { PrismaService } from '@/database/prisma/prisma.service'
+import { Module } from '@nestjs/common'
+import { PostsPrismaRepository } from './repositories/posts-prisma.repository'
 
-@Module({})
+@Module({
+  imports: [DatabaseModule],
+  providers: [
+    {
+      provide: 'PrismaService',
+      useClass: PrismaService,
+    },
+    {
+      provide: 'PostsRepository',
+      useFactory: (prisma: PrismaService) => {
+        return new PostsPrismaRepository(prisma)
+      },
+      inject: ['PrismaService'],
+    },
+  ],
+})
 export class PostsModule {}
